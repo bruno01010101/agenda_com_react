9eebebd6576bbbd53f2ea59417a91a45452726bf
Bruno dos santos <bruno16082006@gmail.com>
2026-04-01 17:09:34 -0300

    começando o front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route>
+        
+      </Route>
+    </BrowserRouter>
+  )
+}
+
+export default App
