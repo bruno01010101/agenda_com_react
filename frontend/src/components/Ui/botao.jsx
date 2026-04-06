eac572575b4143849a238ca69ec5c31cc345c29d
Bruno dos santos <bruno16082006@gmail.com>
2026-04-06 16:26:32 -0300

    exclui tudo otário kkkk, so não faz o pull antes de tirar print

--- frontend/src/components/Ui/botao.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./botao.module.css";
-export default function Botao({text, size, handleClick}){
-    const name = `btn_${size}`
-    return(
-        <button className={styles[name]} onClick={handleClick}>{text}</button>
-    )
-}
